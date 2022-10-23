acd0c499b8b5af5e177539e81e3789ebbda9df7d
Rachel Pokroy <213721715>
2022-10-23 12:47:53 +0300

    lesson0 stage 6

---
+++ stage0/Program1715.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace stage0
+{
+   partial class P
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Welcome1715();
+            Console.ReadKey();
+        }
+        static partial void Welcome0965();
+        private static void Welcome1715()
+        {
+            Console.WriteLine("Enter your name: ");
+            string v = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", v);
+        }
+       
+    }
+}
