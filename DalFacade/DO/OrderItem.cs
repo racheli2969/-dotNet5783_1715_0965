c15e4e75f9ca54f6afc94ff7f6020bb0170f4892
פוקרוי רחל < @J16>
2022-11-02 13:35:24 +0200

    added order and order items class

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -1,4 +1,11 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
 namespace DO;
 /// <summary>
 /// defines the item details
