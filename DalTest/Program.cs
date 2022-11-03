5d1902c6c53dcbc78cfb0ba3c1ce457437afe21f
rachelpokroy@outlook.com <rachelpokroy@outlook.com>
2022-11-03 23:01:57 +0200

    middle

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,14 +1,10 @@
 ﻿using Dal;
-using DalFacade;
-using DO;
-using Enums;
-using System;
 
-public void PrintOptions(){
+ void PrintOptions(){
     Console.WriteLine("To Exit:0,\r\n  To  Add: a,\r\n  To View By Id: b,\r\n   To View All:c,\r\n  To Update: d,\r\n   To Delete: e");
 }
  
-public void Add(int characterType){
+ void Add(int characterType){
     switch(characterType){
         case OptionsForMain.Item:
             break;
@@ -18,7 +14,7 @@ public void Add(int characterType){
             break;
     }
 }
-public void ViewById(int characterType)
+ void ViewById(int characterType)
 {
 switch(characterType){
         case OptionsForMain.Item:
@@ -29,7 +25,7 @@ switch(characterType){
             break;
 }
   }            
-public void ViewAll(int characterType)
+ void ViewAll(int characterType)
 {
 switch(characterType){
         case OptionsForMain.Item:
@@ -40,7 +36,7 @@ switch(characterType){
             break;
 }
   }             
-public void Update(int characterType)
+ void Update(int characterType)
 {
 switch(characterType){
         case OptionsForMain.Item:
@@ -51,7 +47,7 @@ switch(characterType){
             break;
 }
 }
-public void Delete(int characterType)
+ void Delete(int characterType)
 {
 switch(characterType){
         case OptionsForMain.Item:
@@ -63,44 +59,45 @@ switch(characterType){
 }
 }               
 
-public void ControlOptions(int characterType){
+ void ControlOptions(int characterType){
     PrintOptions();
     int x = int.Parse(Console.ReadLine());
-    while (x){
+    while (x!=0)
+    {
         PrintOptions();
         switch (x)
         {
             case OptionsOfActions.Exit:
                 break;
             case OptionsOfActions.AddItem:
-            Add(characterType);
+                Add(characterType);
                 break;
             case OptionsOfActions.ViewById:
-            ViewById(characterType);
+                ViewById(characterType);
                 break;
             case OptionsOfActions.ViewAll:
-            ViewAll(characterType);
+                ViewAll(characterType);
                 break;
-                case OptionsOfActions.UpdateItem:
-            Update(characterType);
+            case OptionsOfActions.UpdateItem:
+                Update(characterType);
                 break;
-                case OptionsOfActions.DeleteItem:
-           Delete(characterType);
+            case OptionsOfActions.DeleteItem:
+                Delete(characterType);
                 break;
-                Exit=0,
-}
+
         }
-
     }
+
 }
 
-public static void Main(){
+
+static void Main(){
     Console.WriteLine("For Item Enter 1\nFor Order Enter 2\nFor Order Item Enter 3\nTo Exit Enter 0\n");
     int x = int.Parse(Console.ReadLine());
     while (x < 0 || x > 3)
     {
         Console.WriteLine("Input needs to be between 0 and 3\nPlease try again");
-        int x = int.Parse(Console.ReadLine());
+        x = int.Parse(Console.ReadLine());
     }
 if(x!=0)
         ControlOptions(x);
