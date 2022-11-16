db60b86c45d867cdd004f5d240ae64f958596043
פוקרוי רחל < @J16>
2022-11-16 18:43:11 +0200

    started 5

---
+++ BL/BO/Order.cs
@@ -0,0 +1,17 @@
+﻿
+namespace BO;
+public class Order
+{
+    // public ... { set; get; }
+
+    //copied from DO
+//    public override string ToString() => $@"
+//Order ID={OrderId}, Customer Name: {CustomerName}, 
+//Email: {Email},
+//    	Address: {Address},
+//    	Date Ordered: {DateOrdered},
+//	Date Delivered: {DateDelivered},
+//	Date Received: {DateReceived}
+//";
+
+}
