834c6b7d5e1cf2caffb72e09c31eca1a9295459a
rachelpokroy@outlook.com <rachelpokroy@outlook.com>
2022-11-27 16:24:36 +0200

    added /// to exceptions

--- BL/BlApi/Exceptions.cs
+++ BL/BlApi/Exceptions.cs
@@ -1,35 +1,52 @@
 ﻿
 namespace BlApi;
 
-
+/// <summary>
+/// Exception for not found
+/// </summary>
 public class ProductNotFoundException : Exception
 {
     public override string Message => "Book not found";
 }
+/// <summary>
+/// Exception for duplicated value
+/// </summary>
 public class ProductExistsException : Exception
 {
     public override string Message => "Product already exists";
 
 }
+/// <summary>
+/// Exception for missing info
+/// </summary>
 public class EmptyStringException : Exception
 {
     public override string Message => "please fill in all the details";
 }
+/// <summary>
+/// Exception for negative id value
+/// </summary>
 public class NegativeIdException: Exception
 {
     public override string Message => " Id is a positive number";
 }
-
+/// <summary>
+/// Exception for negative price value
+/// </summary>
 public class NegativePriceException : Exception
 {
     public override string Message => " Price is a positive number";
 }
-
-public class NegativeAmountdException : Exception
+/// <summary>
+/// Exception for negative amount value
+/// </summary>
+public class NegativeAmountException : Exception
 {
     public override string Message => " Amount is a positive number";
 }
-
+/// <summary>
+/// Exception for not in stock
+/// </summary>
 public class NotInStockException : Exception
 {
     public override string Message => "Sorry, not in stock";
