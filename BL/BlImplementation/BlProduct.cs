7b5c2d2fe26cda6e57fd3537cb94871f90661deb
גרשון רחל < @J17>
2022-11-28 14:55:46 +0200

    JJ

---
+++ BL/BlImplementation/BlProduct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlApi;
+namespace BL.BlImplementation
+{
+
+    internal class BlProduct : IProduct
+    {
+        private DalApi.IDal dal = new DalList();
+        public IEnumerable<BO.ProductForList> GetProductList()
+        {
+            return
+        }
+        public BO.Product GetProductForManager(int id)
+        {
+            return
+        }
+        public BO.ProductItem GetProductForCustomer(int id)
+        {
+            return
+        }
+        public void AddProduct(BO.Product p) {
+        }
+    }
+}
