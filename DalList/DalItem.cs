d131cf87ea572fdfed1c3ee01e26b4191a4f60dc
rachelpokroy@outlook.com <rachelpokroy@outlook.com>
2022-11-30 21:58:42 +0200

    jj

--- DalList/DalItem.cs
+++ DalList/DalItem.cs
@@ -22,7 +22,7 @@ internal class DalItem : IItem
     /// </summary>
     /// <param name="Id"></param>
     /// <returns>returns the item</returns>
-    /// <exception cref="Exception"></exception>
+    /// <exception cref="Exception">not found</exception>
     public Item GetById(int Id)
     {
         for (int i = 0; i < DataSource.Items.Count; i++)
@@ -92,11 +92,11 @@ internal class DalItem : IItem
     public bool Available(int id)
     {
         Item item = GetById(id);
-        return item.AmountInStock - 1 >= 0;
+        return item.InStock - 1 >= 0;
     }
     public bool Available(int id, int amount)
     {
         Item item = GetById(id);
-        return item.AmountInStock - amount >= 0;
+        return item.InStock - amount >= 0;
     }
 }
