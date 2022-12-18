a0e18fe70b0f69378ed9de1d4b32af1228e4d0e2
גרשון רחל < @J17>
2022-12-18 13:35:29 +0200

    middle of four

--- BL/BlApi/Factory.cs
+++ BL/BlApi/Factory.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BL.BlApi
+﻿namespace BlApi;
+public static class Factory
 {
-    internal class Factory
+    public static IBl MyProperty
     {
-    }
+        get;
+    } = new BlImplementation.Bl();
 }
+
