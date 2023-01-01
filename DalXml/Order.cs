6081a2d9a6c53c8f7d3dd8599da860bc34f620e7
rachelpokroy@outlook.com <rachelpokroy@outlook.com>
2023-01-01 15:06:17 +0200

    tryed add of order using serialize

--- DalXml/Order.cs
+++ DalXml/Order.cs
@@ -1,12 +1,30 @@
 ﻿using DalApi;
+using DO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace Dal;
 
 internal class Order : IOrder
 {
-    public int Add(DO.Order item)
+    private XElement? ordersXml = XDocument.Load(@"..\..\..\..\xml\Order.xml").Root;
+    public int Add(DO.Order order)
     {
-        throw new NotImplementedException();
+        XmlSerializer ser = new XmlSerializer(typeof(Order));
+        StreamWriter w = new StreamWriter(@"..\..\..\..\xml\Order.xml");
+
+        //the id from config can be shortened?
+        XElement? configXml = XDocument.Load(@"..\..\..\..\xml\Config.xml").Root;
+        string? id = (string?)(configXml?.Element("OrderId"));  
+        order.OrderId = Convert.ToInt32(id);
+        id = (order.OrderId + 1).ToString();
+        configXml?.Element("ItemId")?.SetValue(id);
+        configXml?.Save(@"..\..\..\..\xml\Config.xml"); 
+
+        ser.Serialize(w, order);
+        w.Close();
+        return order.OrderId;
+
     }
 
     public void Delete(int id)
