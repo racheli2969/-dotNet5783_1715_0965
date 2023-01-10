cc3a92eb1ac73d001bc695aec0f54baac12fa23f
פוקרוי רחל < @J16>
2023-01-10 12:28:35 +0200

    orderlist window

--- PL_/MainWindow.xaml.cs
+++ PL_/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace PL
 {
@@ -31,9 +18,9 @@ namespace PL
 
         private void BtnEnter_Click(object sender, RoutedEventArgs e)
         {
-            ProductListWindow PL = new(Bl);
+            ProductListWindow PL = new(b: Bl??null,this);
             PL.Show();
-            this.Close();
+            this.Hide();
         }
     }
 }
