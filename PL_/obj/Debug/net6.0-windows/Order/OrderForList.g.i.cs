487d2921c79f4c3aeff744f7107ec6d35837575d
rachelpokroy@outlook.com <rachelpokroy@outlook.com>
2023-02-21 11:54:55 +0200

    0

--- PL_/obj/Debug/net6.0-windows/Order/OrderForList.g.i.cs
+++ PL_/obj/Debug/net6.0-windows/Order/OrderForList.g.i.cs
@@ -87,13 +87,13 @@ namespace PL_.Order {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PL_;component/order/orderforlist.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PL_;V1.0.0.0;component/order/orderforlist.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Order\OrderForList.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -103,7 +103,7 @@ namespace PL_.Order {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
