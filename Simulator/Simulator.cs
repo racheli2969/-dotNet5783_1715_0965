90ce6b9034d763697015a188735e7183134ff6ad
פוקרוי רחל < @J16>
2023-02-22 15:57:04 +0200

    bears

--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -8,25 +8,22 @@ public static class Simulator
 {
     private static readonly IBl? Bl = Factory.Get();
     private static Thread thread = new(Run);
-    private static volatile bool isRunning=true;
+    private static volatile bool isRunning = true;
 
     public static void Run()
     {
-        while (isRunning)
+        int? id = Bl?.Order.GetOldestOrderNumber();
+        while (id != null && isRunning)
         {
-            int? id = Bl?.Order.GetOldestOrderNumber();
-            while (id != null)
-            {
-                BO.Order? order = new();
-                order = Bl?.Order.GetOrderDetails((int)id);
-                if (order?.DateShipped == DateTime.MinValue)
-                    order = Bl?.Order.UpdateOrderShipping((int)id);
-                else if (order?.DateDelivered == DateTime.MinValue) 
-                    order = Bl?.Order.UpdateOrderDelivery((int)id);
-                // ((int)id).Invoke(OrderInProgress);
+            BO.Order? order = new();
+            order = Bl?.Order.GetOrderDetails((int)id);
+            if (order?.DateShipped == DateTime.MinValue)
+                order = Bl?.Order.UpdateOrderShipping((int)id);
+            else if (order?.DateDelivered == DateTime.MinValue)
+                order = Bl?.Order.UpdateOrderDelivery((int)id);
+            // ((int)id).Invoke(OrderInProgress);
 
-                id = Bl?.Order.GetOldestOrderNumber();
-            } 
+            id = Bl?.Order.GetOldestOrderNumber();
         }
     }
     public static void Stop()
