2d6d2e4ab6ee1c6974abf304dbffa97b3ed47dee
גרשון רחל < @J17>
2023-02-28 11:00:36 +0200

    2

--- BL/BlImplementation/BLCart.cs
+++ BL/BlImplementation/BLCart.cs
@@ -14,48 +14,43 @@ internal class BLCart : BlApi.ICart
         //check if product exists if so get product
         try
         {
-            lock (dal)
+            DO.Item? product = dal?.Item?.GetAll(x => x.ID == productId)?.ToList().FirstOrDefault();
+            //if not available
+            if (dal?.Item?.Available(productId) == false)
+                throw new BlApi.NotInStockException();
+            //check if the item is in the cart already
+            int idx = ProductIndexInCart(c, productId);
+            if (idx >= 0)
             {
-                DO.Item? product = dal?.Item?.GetAll(x => x.ID == productId)?.ToList().FirstOrDefault();
-
-                //if not available
-                if (dal?.Item?.Available(productId) == false)
-                    throw new BlApi.NotInStockException();
-                //check if the item is in the cart already
-                int idx = ProductIndexInCart(c, productId);
-                if (idx >= 0)
-                {
-                    //update the amount in the cart                 
-                    ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].Amount++;
-                    ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].PriceOfItems
-                        += ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].ItemPrice;
-                }
-
-                else
-                {    //add the product
-                    BO.OrderItem oi = new BO.OrderItem();
-                    if (product != null)
-                    {
-                        oi.ItemId = product.Value.ID;
-                        oi.ItemName = product.Value.Name;
-                        oi.ItemPrice = product.Value.Price;
-                        oi.Amount = 1;
-                        oi.PriceOfItems = product.Value.Price;
-                        if (c != null && c.Items == null)
-                        {
-                            List<BO.OrderItem> temp = new();
-                            temp.Add(oi);
-                            c.Items = temp;
-                        }
-                        else
-                            c?.Items?.Add(oi);
-                    }
-                }
-                if (c?.Items != null)
-                    c.FinalPrice = c.Items.Select(i => i.ItemPrice).Sum();
-                //return updated cart
-                return c ?? throw new BlApi.BlNOtImplementedException();
+                //update the amount in the cart                 
+                ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].Amount++;
+                ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].PriceOfItems
+                    += ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].ItemPrice;
             }
+            else
+            {    //add the product
+                BO.OrderItem oi = new BO.OrderItem();
+                if (product != null)
+                {
+                    oi.ItemId = product.Value.ID;
+                    oi.ItemName = product.Value.Name;
+                    oi.ItemPrice = product.Value.Price;
+                    oi.Amount = 1;
+                    oi.PriceOfItems = product.Value.Price;
+                    if (c != null && c.Items == null)
+                    {
+                        List<BO.OrderItem> temp = new();
+                        temp.Add(oi);
+                        c.Items = temp;
+                    }
+                    else
+                        c?.Items?.Add(oi);
+                }
+            }
+            if (c?.Items != null)
+                c.FinalPrice = c.Items.Select(i => i.ItemPrice).Sum();
+            //return updated cart
+            return c ?? throw new BlApi.BlNOtImplementedException();
         }
         catch (DalApi.EntityNotFoundException)
         {
@@ -96,23 +91,19 @@ internal class BLCart : BlApi.ICart
         //if the added amount is bigger then the current amount then needed to check if there is enough in stock
         else
         {
-            lock (dal)
+            if (dal?.Item?.Available(productId, quantity) == false)
             {
-                if (dal?.Item?.Available(productId, quantity) == false)
-                {
-                    throw new BlApi.NotInStockException();
-                }
+                throw new BlApi.NotInStockException();
+            }
              //if in stock add the difference to the cart then calculates the final price 
              (c ?? throw new BlApi.BlNOtImplementedException()).FinalPrice
-                 += ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].ItemPrice
-                 * (quantity - (((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].Amount));
+                 += ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].ItemPrice * (quantity - (((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].Amount));
 
-                ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].Amount = quantity;
+            ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].Amount = quantity;
 
-                ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].PriceOfItems
-                    = quantity
-                    * ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].ItemPrice;
-            }
+            ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].PriceOfItems
+                = quantity
+                * ((c ?? throw new BlApi.BlNOtImplementedException()).Items ?? throw new BlApi.BlNOtImplementedException())[idx].ItemPrice;
         }
         return c;
     }
@@ -140,25 +131,22 @@ internal class BLCart : BlApi.ICart
         //send to dal
         if (dal != null)
         {
-            lock (dal)
-            {
-                int id = dal.Order.Add(temp);
+            int id = dal.Order.Add(temp);
 
-                //add the items to the order item array and update the products accordingly
-                DO.OrderItem tempItem = new DO.OrderItem();
-                for (int i = 0; i < cart?.Items?.Count; i++)
-                {
-                    //create order items in dal layer 
-                    tempItem.Amount = cart.Items[i].Amount;
-                    tempItem.OrderID = temp.OrderId;
-                    tempItem.ItemId = cart.Items[i].ItemId;
-                    //adds to order items
-                    id = dal.OrderItem.Add(tempItem);
-                    tempItem.OrderItemId = id;
-                    cart.Items[i].OrderItemId = id;
-                    //updates amount
-                    dal.Item.Update(tempItem.ItemId, tempItem.Amount);
-                }
+            //add the items to the order item array and update the products accordingly
+            DO.OrderItem tempItem = new DO.OrderItem();
+            for (int i = 0; i < cart?.Items?.Count; i++)
+            {
+                //create order items in dal layer 
+                tempItem.Amount = cart.Items[i].Amount;
+                tempItem.OrderID = temp.OrderId;
+                tempItem.ItemId = cart.Items[i].ItemId;
+                //adds to order items
+                id = dal.OrderItem.Add(tempItem);
+                tempItem.OrderItemId = id;
+                cart.Items[i].OrderItemId = id;
+                //updates amount
+                dal.Item.Update(tempItem.ItemId, tempItem.Amount);
             }
         }
     }
@@ -178,16 +166,13 @@ internal class BLCart : BlApi.ICart
 
     private void validateItem(BO.OrderItem oi)
     {
-        lock (dal)
-        {
-            //product exists
-            dal?.Item?.GetAll(o => o.ID == oi.ItemId)?.ToList();
-            //amount is a positive number
-            if (oi.Amount <= 0)
-                throw new BlApi.NegativeAmountException();
-            //item is in stock
-            if (dal?.Item?.Available(oi.ItemId, oi.Amount) == false)
-                throw new BlApi.NotInStockException();
-        }
+        //product exists
+        dal?.Item?.GetAll(o => o.ID == oi.ItemId)?.ToList();
+        //amount is a positive number
+        if (oi.Amount <= 0)
+            throw new BlApi.NegativeAmountException();
+        //item is in stock
+        if (dal?.Item?.Available(oi.ItemId, oi.Amount) == false)
+            throw new BlApi.NotInStockException();
     }
 }
